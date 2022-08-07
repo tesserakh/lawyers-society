44346fdc2b65b55a1a8ba495a16e18a4a9d2cf25
akherlan <herlandi.27@gmail.com>
2022-08-07 17:58:50 +0700

    1st scrape, separate links into 9 files

--- scrape.py
+++ scrape.py
@@ -18,8 +18,12 @@ class LawyersSpider(scrapy.Spider):
     
     def parse(self, response):
         profile_info = response.css('div.cell.small-12.medium-6').css('div.member-info-wrapper')
+        special_info = response.css('div.member-special-cases')
+        
         labels = ['Link']
         values = [response.meta['url']]
+        
+        # Left side content
         for info in profile_info:
             label = info.css('div.member-info-label::text').get().strip()
 
@@ -39,7 +43,22 @@ class LawyersSpider(scrapy.Spider):
             
             labels.append(label)
             values.append(value)
-        
+
+        # Right side content
+        for case in special_info:
+            label = case.css('div.member-info-label::text').get().strip()
+            if len(case.css('div.member-info-value').css('span::text')) == 2:
+                value = case.css('div.member-info-value').css('span::text').getall()
+                value = value[1]
+            else:
+                value = case.css('div.member-info-value').css('span::text').get()
+                if value == None:
+                    value = case.css('p.member-info-value::text').get()
+            value = value.strip()
+
+            labels.append(label)
+            values.append(value)
+
 
         yield { labels[i]: values[i] for i in range(len(labels)) }
 
